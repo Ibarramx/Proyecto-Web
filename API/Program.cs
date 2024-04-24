5cb10d56cb934c29d9b6d5ecd74b893136ef6bad
Jonathan Moroyoqui <jonathan.moroyoqui77@gmail.com>
2024-04-24 09:21:18 -0700

    API

--- API/Program.cs
+++ API/Program.cs
@@ -1,11 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
+// Add Swagger/OpenAPI services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Configurar políticas CORS
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("PoliticasCors", builder =>
+    {
+        builder.AllowAnyOrigin() // Permite cualquier origen
+               .AllowAnyMethod() // Permite cualquier método HTTP
+               .AllowAnyHeader(); // Permite cualquier encabezado HTTP
+    });
+});
 
 var app = builder.Build();
 
@@ -17,6 +28,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Habilitar CORS
+app.UseCors("PoliticasCors");
 
 app.UseAuthorization();
 
