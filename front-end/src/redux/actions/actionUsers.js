76da3969a582bdcd6992031cb7e5abbe52de9903
Jonathan Moroyoqui <jonathan.moroyoqui77@gmail.com>
2024-05-04 15:44:07 -0700

    se agrego editUser

--- front-end/src/redux/actions/actionUsers.js
+++ front-end/src/redux/actions/actionUsers.js
@@ -31,6 +31,21 @@ export const getUserUnique = createAsyncThunk("users/getUserUnique",
     }
 );
 
+export const deleteUser = createAsyncThunk("users/deleteUser",
+    async (id, {rejectWithValue}) => {
+        try
+        {      
+            const resp = await axios.delete('http://187.189.158.186:7777/Usuario/'+id);
+
+            return resp.data;
+        } 
+        catch (error) 
+        {
+            return rejectWithValue(`Error: ${error.message}`);
+        }
+    }
+);
+
 export const addUser = createAsyncThunk("users/addUser",
     async (data, {rejectWithValue}) => {
         try
@@ -46,11 +61,11 @@ export const addUser = createAsyncThunk("users/addUser",
     }
 );
 
-export const deleteUser = createAsyncThunk("users/deleteUser",
-    async (id, {rejectWithValue}) => {
+export const editUSer = createAsyncThunk("users/editUSer",
+    async (data, {rejectWithValue}) => {
         try
         {      
-            const resp = await axios.delete('http://187.189.158.186:7777/Usuario/'+id);
+            const resp = await axios.put('http://187.189.158.186:7777/Usuario/'+data.id, data);
 
             return resp.data;
         } 
